38b7788868fe925ba97e272528b5bfe8544d23c7
Jesse Byers <jesse.s.byers@gmail.com>
2020-07-15 10:37:44 -0400

    flowers appearing on DOM

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import Flowers from './Components/Flowers'
+import { drawFlowers } from './d3/DrawFlowers'
+
+// import Flowers from './Components/Flowers'
 
 function App() {
 
@@ -24,6 +26,7 @@ function App() {
          
           const response = await fetch(apiUrl, requestOptions)
           const data = await response.json()
+          drawFlowers(data)
           setDays(data)
           setIsLoading(false);
       }
@@ -32,15 +35,14 @@ function App() {
 
   return (
 
-    <div>
-      {isLoading ? (
-          <h1>Loading</h1>
-      ) : (
-        <Flowers data={days}/>
-      )}
-    </div>
-
-    
+    // <div>
+    //   {isLoading ? (
+    //       <h1>Loading</h1>
+    //   ) : (
+        <div className="viz">
+        </div>
+    //   )}
+    // </div>
   );
 }
 
--- src/d3/DrawFlowers.js
+++ src/d3/DrawFlowers.js
@@ -19,54 +19,52 @@ export const drawFlowers = (days) => {
         const svg = d3.select('.viz')
             // svg.html`<svg width="500" height="500"><path transform="translate(25,50)" d="${petalPath}"></svg>`
             // svg.html`<svg width="500" height="500"><path transform="translate(25,50)" d='M 0,0 C -25,-10 -5,-40 0,-50 C 5,-40 25,-10 0,0'></svg>`
-
+            .append('svg')
             // .attr('viewbox', [0, 0, petalSize * 10, petalSize * 10])
             .attr('height', height)
             .attr('width', width)
 
 
 
-        // const tempMinmax = d3.extent(data, d => d.temp.day);
+        const tempMinmax = d3.extent(data, d => d.temp.day);
 
-        // const windMinmax = d3.extent(data, d => d.wind_speed);
+        const windMinmax = d3.extent(data, d => d.wind_speed);
       
-        // const sizeScale = d3.scaleLinear().domain(windMinmax).range([0.25, 1]);
-        // const numPetalScale = d3.scaleQuantize().domain(tempMinmax).range([3, 6, 9, 12]);      
+        const sizeScale = d3.scaleLinear().domain(windMinmax).range([0.25, 1]);
+        const numPetalScale = d3.scaleQuantize().domain(tempMinmax).range([3, 6, 9, 12]);      
       
-        // const flowersData = _.map(data, d => {
-        // //   const flowersData = data.map(d => {
-
-        //   const numPetals = numPetalScale(d.temp.day);
-        // //   const petSize = sizeScale(d.wind_speed);
-        //   return {
-        //     // petSize,
-        //     petals: _.times(numPetals, i => {
-        //       return {
-        //         angle: 360 * i / numPetals, 
-        //         petalPath
-        //       }
-        //     }),
-        //     numPetals,
-        //   }
-        // })
-        // console.log(flowersData)
+        const flowersData = _.map(data, d => {
+          const numPetals = numPetalScale(d.temp.day);
+          const petSize = sizeScale(d.wind_speed);
+          return {
+            petSize,
+            petals: _.times(numPetals, i => {
+              return {
+                angle: 360 * i / numPetals, 
+                petalPath
+              }
+            }),
+            numPetals,
+          }
+        })
+        console.log(flowersData)
         
-        // const flowers = d3.select(svg)
-        //   .selectAll('g')
-        //   .data(flowersData)
-        //   .enter()
-        //   .append('g')
-        //   .attr('transform', (d, i) => `translate(${(i % 5) * petalSize}, ${Math.floor(i / 5) * petalSize})scale(${d.petSize})`);
+        const flowers = d3.select('svg')
+          .selectAll('g')
+          .data(flowersData)
+          .enter()
+          .append('g')
+          .attr('transform', (d, i) => `translate(${(i % 5) * petalSize}, ${Math.floor(i / 5) * petalSize})scale(${d.petSize})`);
       
-        //   console.log(flowers)
+          console.log(flowers)
         
-        // flowers.selectAll('path')
-        //   .data(d => d.petals)
-        //   .enter()
-        //   .append('path')
-        //   .attr('d', d => d.petalPath)
-        //   .attr('transform', d => `rotate(${d.angle})`)
-        //   .attr('fill', (d, i) => d3.interpolateWarm(d.angle / 360));
+        flowers.selectAll('path')
+          .data(d => d.petals)
+          .enter()
+          .append('path')
+          .attr('d', d => d.petalPath)
+          .attr('transform', d => `rotate(${d.angle})`)
+          .attr('fill', (d, i) => d3.interpolateWarm(d.angle / 360));
         
         return svg
     
